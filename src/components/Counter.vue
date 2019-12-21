8d1263376384b6488541b1925905a5323746a796
arman980 <arman.gasparyan5.y@tumo.org>
2019-12-21 14:07:03 +0400

    ex.2 finished

---
+++ src/components/Counter.vue
@@ -0,0 +1,24 @@
+        <template>
+        <v-container>
+        <v-btn v-bind:color="color"><slot></slot>Counter: {{ count }}</v-btn>
+        <v-btn v-on:click="add" v-bind:color="color">Counter: {{ count }} <slot></slot></v-btn>
+        </v-container>
+        </template>
+
+
+        <script>
+        export default {        
+        name: 'Counter',
+        data: () => ({
+            count: 0
+        }),
+        props: {
+            color: String
+        },
+        methods: {
+            add: function () {
+                this.count++
+            }
+        }
+        }
+        </script>
